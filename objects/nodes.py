591195a47db0419cef875f179a9432287b9fa18e
Beniamin <piaskupiasku@gmail.com>
2019-03-01 20:33:36 +0100

    created first support and node objects and prototype of project tree

---
+++ objects/nodes.py
@@ -0,0 +1,18 @@
+from .supports import Free
+
+
+# TODO: maybe it should be an abstract class?
+class Node:
+    def _set_localid(self, id):
+        self._localid = id
+
+    def _set_globalid(self, bar_id):
+        self._globalid = bar_id + self._localid
+
+
+class Node2D(Node):
+    _dim = '2D'
+
+    def __init__(self, x=0, y=0, support=Free):
+        self.coordinates = (x, y)
+        self.support = support
---
+++ objects/supports.py
@@ -0,0 +1,12 @@
+class Support:
+    '''
+    Support class is containing information about directions
+    in which construction can move or rotate in a given node.
+    '''
+    pass
+
+
+class Free(Support):
+    x_dir = (False, False)
+    y_dir = (False, False)
+    z_dir = (False, False)
