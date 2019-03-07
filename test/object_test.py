82b31f48e8509358d7df81ca9f8ea80487cc0ac6
thinkofher <piaskupiasku@gmail.com>
2019-03-08 00:54:32 +0100

    added material tests

--- test/object_test.py
+++ test/object_test.py
@@ -2,6 +2,38 @@ import unittest
 from math import sqrt
 from ..src.objects.bar import Bar
 from ..src.objects.nodes import Node2D
+from ..src.objects.materials.material import ElasticMaterial
+
+
+class TestMaterials(unittest.TestCase):
+
+    _steel_young_m = 210e9
+    _steel_pos = 0.3
+    steel = ElasticMaterial(
+        young_m=_steel_young_m, poisson=_steel_pos)
+
+    _aluminium_kirhf_m = 25.5
+    _aluminium_pos = 0.33
+    aluminium = ElasticMaterial(
+        kirchff_m=_aluminium_kirhf_m, poisson=_aluminium_pos)
+
+    _concrete_young_m = 27e9
+    _concrete_kirhf_m = 11.25e9
+    concrete = ElasticMaterial(
+        young_m=_concrete_young_m, kirchff_m=_concrete_kirhf_m)
+
+    def test_kirchoff_modulus(self):
+        steel_kirch_modulus = self._steel_young_m/(2*(1+self._steel_pos))
+        self.assertEqual(self.steel.kirchoffs_modulus, steel_kirch_modulus)
+
+    def test_young_modulus(self):
+        alum_young_modulus = 2*self._aluminium_kirhf_m*(1+self._aluminium_pos)
+        self.assertEqual(self.aluminium.youngs_modulus, alum_young_modulus)
+
+    def test_poissons_ratio(self):
+        conc_pos_ratio = ((self._concrete_young_m-2*self._concrete_kirhf_m) /
+                          (2*self._concrete_kirhf_m))
+        self.assertEqual(self.concrete.poissons_ratio, conc_pos_ratio)
 
 
 class TestLengthOfBarObjects(unittest.TestCase):
