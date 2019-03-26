a90fb6ba67a982fb977135eb14fc5531c38909c2
thinkofher <piaskupiasku@gmail.com>
2019-03-27 00:38:52 +0100

    First approach to make imports clear and readable

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
+from .src.objects.bar import Bar
+from .src.objects.materials.material import ElasticMaterial
+from .src.objects.sections import CircleBar
+from .src.objects.nodes import Node2D
+from .src.objects.truss import TrussBar, TrussConstruction
 
+__all__ = ['Node2D', 'Bar', 'ElasticMaterial', 'CircleBar',
+           'TrussBar', 'TrussConstruction']
