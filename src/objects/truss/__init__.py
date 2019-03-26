aa90cce969dd6bc7ac2ab18f0db884aad2a06369
thinkofher <piaskupiasku@gmail.com>
2019-03-27 00:43:15 +0100

    Added docstrings and created global stifness matrix property.

---
+++ src/objects/truss/__init__.py
@@ -0,0 +1,4 @@
+from .general import TrussBar
+from .construction import TrussConstruction
+
+__all__ = ['TrussBar', 'TrussConstruction']
--- src/objects/truss/general.py
+++ src/objects/truss/general.py
@@ -9,11 +9,15 @@ class _TrussMatrixes:
 
     @property
     def local_stifness_matrix(self):
+        """
+        Returns stifness matrix of trusses bar element in
+        local coordinate system.
+        """
         L = self.length
         A = self.section.area
         E = self.material.youngs_modulus
 
-        return (A*E*(1/L))*array([
+        return ((A*E)/L)*array([
             [1, 0, -1, 0],
             [0, 0, 0, 0],
             [-1, 0, 1, 0],
@@ -22,6 +26,9 @@ class _TrussMatrixes:
 
     @property
     def transformation_matrix(self):
+        """
+        Returns transformation matrix of trusses bar element.
+        """
         s = self.sin_of_angle
         c = self.cos_of_angle
 
@@ -34,7 +41,14 @@ class _TrussMatrixes:
 
     @property
     def global_stifness_matrix(self):
-        pass
+        """
+        Returns stifness matrix of trusses bar element in
+        global coordinate system.
+        """
+        theta = self.transformation_matrix
+        k = self.local_stifness_matrix
+
+        return theta.transpose() @ k @ theta
 
 
 class TrussBar(Bar, _TrussMatrixes):
